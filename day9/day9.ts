d48c85564912de6ffe215a75b4774c7b48d36b7b
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-12-09 13:52:19 +0100

    complete day 9

---
+++ day9/day9.ts
@@ -0,0 +1,85 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+type Point = [number, number];
+
+function createRope(length: number): Point[] {
+  const rope: Point[] = [];
+  for (let i = 0; i < length; i++) {
+    rope[i] = [0, 0];
+  }
+  return rope;
+}
+
+// Calculates the tail position after a head move.
+// Assumes that the tail position was correct before the head moved.
+function handleKnotMove(head: Point, tail: Point): Point {
+  const vector = [head[0] - tail[0], head[1] - tail[1]];
+  let moveVector = [0, 0];
+  if (Math.abs(vector[0]) > 1 || Math.abs(vector[1]) > 1) {
+    moveVector = vector.map((d) => (Math.abs(d) > 1 ? d - Math.sign(d) : d));
+  }
+  return [tail[0] + moveVector[0], tail[1] + moveVector[1]];
+}
+
+function updateRope(rope: Point[]): void {
+  for (let i = 1; i < rope.length; i++) {
+    rope[i] = handleKnotMove(rope[i - 1], rope[i]);
+  }
+}
+
+function getTailPositionString(rope: Point[]): string {
+  return `${rope[rope.length - 1][0]},${rope[rope.length - 1][1]}`;
+}
+
+async function day9(): Promise<void> {
+  const inputFile = fs.createReadStream('./day9/input.txt');
+  const inputLines = readline.createInterface({
+    input: inputFile,
+    crlfDelay: Infinity,
+  });
+
+  const rope1 = createRope(2);
+  const rope2 = createRope(10);
+
+  const tailMap1: Record<string, true> = { '0,0': true };
+  const tailMap2: Record<string, true> = { '0,0': true };
+
+  for await (const inputLine of inputLines) {
+    const [direction, m] = inputLine.split(' ');
+    const moves = parseInt(m);
+    for (let i = 0; i < moves; i++) {
+      switch (direction) {
+        case 'U':
+          rope1[0][1]++;
+          rope2[0][1]++;
+          break;
+        case 'R':
+          rope1[0][0]++;
+          rope2[0][0]++;
+          break;
+        case 'D':
+          rope1[0][1]--;
+          rope2[0][1]--;
+          break;
+        case 'L':
+          rope1[0][0]--;
+          rope2[0][0]--;
+          break;
+      }
+      updateRope(rope1);
+      updateRope(rope2);
+      tailMap1[getTailPositionString(rope1)] = true;
+      tailMap2[getTailPositionString(rope2)] = true;
+    }
+  }
+
+  console.log(
+    `Short rope's tail visited ${Object.keys(tailMap1).length} positions`,
+  );
+  console.log(
+    `Long rope's tail visited ${Object.keys(tailMap2).length} positions`,
+  );
+}
+
+day9();
